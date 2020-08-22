80a57059610640e0c0cf27af2b68f9186952e8c1
Steven Daniel <dstevie9292@gmail.com>
2020-08-22 05:42:38 +0100

    deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 const server = http.createServer(app);
 
